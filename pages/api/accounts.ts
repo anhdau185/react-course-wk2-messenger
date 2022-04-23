6541d367291f758798855058cde35410c10dd57b
Anh Dau <anh.dau@employmenthero.com>
2022-04-23 15:11:39 +0700

    chore: refactor accounts api route

--- pages/api/accounts.ts
+++ pages/api/accounts.ts
@@ -1,22 +1,21 @@
-import { db } from "data";
-import { init } from "data/repository";
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function AccountsAPI(req: NextApiRequest, res: NextApiResponse) {
+import { db } from 'data';
+import { init } from 'data/repository';
+
+async function AccountsAPI(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
-    case "GET":
-      return listAccounts(req, res);
+    case 'GET':
+      const accounts = await getAllAccounts();
+      return res.status(200).json(accounts);
     default:
       return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
 
-// next handler to get account from query.accountId
-async function listAccounts(req: NextApiRequest, res: NextApiResponse) {
+export async function getAllAccounts() {
   await init();
-
-  // return all accounts from db
-  const accounts = db.data?.users;
-
-  return res.status(200).json(accounts);
+  return db.data?.users ?? []; // return all accounts from db
 }
+
+export default AccountsAPI;
