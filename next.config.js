db94d880d0ca41fd3ee3831297ba682ab6f57db0
Anh Dau <anh.dau@employmenthero.com>
2022-04-25 16:28:27 +0700

    add proxy config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:slug*',
+        destination: 'https://messenger-api.tung.ninja/api/:slug*'
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;
